85ec1494758b0347f5c2eac72e45911da0ba5686
liangjuntang <liangjuntang@mallcoo.cn>
2018-05-21 20:05:12 +0800

    1

---
+++ src/CMSProjectTemplate/CMSProjectTemplateWizard/ChildWizardImpl.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using System.Windows.Forms;
+
+namespace CMSProjectTemplateWizard
+{
+    public class ChildWizardImpl : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            string safeprojectname = RootWizardImpl.GlobalParameters.Where(p => p.Key == "$safeprojectname$").First().Value;
+            var msg = replacementsDictionary["$safeprojectname$"] + " to>> " + safeprojectname;
+            replacementsDictionary["$safeprojectname$"] = safeprojectname;
+
+
+            MessageBox.Show(msg);
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/CMSProjectTemplate/CMSProjectTemplateWizard/RootWizardImpl.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+
+namespace CMSProjectTemplateWizard
+{
+    public class RootWizardImpl : IWizard
+    {
+        private string safeprojectname;
+        private static Dictionary<string, string> globalParameters = new Dictionary<string, string>();
+        public static IEnumerable<KeyValuePair<string, string>> GlobalParameters
+        {
+            get { return globalParameters; }
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            safeprojectname = replacementsDictionary["$safeprojectname$"];
+            globalParameters["$safeprojectname$"] = safeprojectname;
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
